a8285a46c59d4b7002d3b63d5a57c8f2484cfc0c
Alex-M-2013 <alexmach01@outlook.com>
2026-09-13 16:47:19 +0100

    Fix broken GitHub Link animation

--- src/components/GithubLink.jsx
+++ src/components/GithubLink.jsx
@@ -2,7 +2,7 @@ import "../styles/components/GitHubLink.css";
 
 export const GitHubLink = () => {
     return (
-        <a href="https://github.com/Alex-M-2013" id="github-link">
+        <a href="https://github.com/Alex-M-2013/MH-Assistant" id="github-link">
             <img src="/assets/images/github.png" />
         </a>
     );
