9ca7eefd863e39ccdfd23fb78f66a1d3a8f8f1e3
Alex-M-2013 <alexmach01@outlook.com>
2026-09-14 20:20:48 +0100

    Add missing alt to image

--- src/components/GitHubLink.jsx
+++ src/components/GitHubLink.jsx
@@ -3,7 +3,7 @@ import "../styles/components/GitHubLink.css";
 export const GitHubLink = () => {
     return (
         <a href="https://github.com/Alex-M-2013/MH-Assistant" id="github-link">
-            <img src="/assets/images/github.png" />
+            <img src="/assets/images/github.png" alt="GitHub Logo" />
         </a>
     );
 };
